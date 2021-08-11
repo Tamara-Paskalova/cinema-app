934a23b3673d3440bc1d468689b0eb10d5b08b0b
Tamara-Paskalova <forptv@gmail.com>
2021-08-11 14:08:21 +0300

    created cinema-app using hibernate

---
+++ src/main/java/cinema/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,75 @@
+package cinema.dao.impl;
+
+import cinema.dao.ShoppingCartDao;
+import cinema.exception.DataProcessingException;
+import cinema.lib.Dao;
+import cinema.model.ShoppingCart;
+import cinema.model.User;
+import cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Could not create Shopping Cart: "
+                    + shoppingCart + " to DB.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<ShoppingCart> query = session.createQuery("from ShoppingCart sc "
+                    + "left join fetch sc.tickets t "
+                    + "left join fetch t.movieSession ms "
+                    + "left join fetch ms.cinemaHall "
+                    + "left join fetch ms.movie "
+                    + "where sc.user = :user", ShoppingCart.class);
+            query.setParameter("user", user);
+            return query.uniqueResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Could not find Shopping Cart by User: " + user, e);
+        }
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.update(shoppingCart);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Could not create Shopping Cart: " + shoppingCart, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
